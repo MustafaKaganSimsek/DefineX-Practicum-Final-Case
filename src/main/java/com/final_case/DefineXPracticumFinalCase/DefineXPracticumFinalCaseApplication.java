91eb36e39f5082f0e3ca55d0215f57411eefefc7
MustafaKaganSimsek <mustafakagan48@gmail.com>
2023-02-09 21:56:20 +0300

    first

---
+++ src/main/java/com/final_case/DefineXPracticumFinalCase/DefineXPracticumFinalCaseApplication.java
@@ -0,0 +1,13 @@
+package com.final_case.DefineXPracticumFinalCase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefineXPracticumFinalCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefineXPracticumFinalCaseApplication.class, args);
+	}
+
+}
